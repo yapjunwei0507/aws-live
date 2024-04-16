4a9fec8ee6e83aac3792caa0563b0dced0188615
yapjunwei0507 <167164882+yapjunwei0507@users.noreply.github.com>
2024-04-16 13:54:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cjok88o242m4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "yapjunwei-employee"
+customregion = "us-east-1"
+
